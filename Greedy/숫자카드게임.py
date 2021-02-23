bde34891f19e790f24c7d7ae67b658eb07f57ae1
SeungJu <sjoh0704@daum.net>
2021-02-23 10:28:30 +0900

    greedy02

---
+++ Greedy/숫자카드게임.py
@@ -0,0 +1,10 @@
+import sys
+read = sys.stdin.readline
+N, M = map(int, read().split())
+max_of_cards = 0
+for _ in range(N):
+    row = list(map(int, read().split()))
+    min_row = min(row)
+    max_of_cards = max(max_of_cards, min_row)
+print(max_of_cards)
+
