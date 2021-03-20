6b2e99119119c3f9c177eee0acfd485ec6fe6214
SeungJu <sjoh0704@daum.net>
2021-03-20 23:50:51 +0900

    restart 15683

--- Baekjoon/15683.py
+++ Baekjoon/15683.py
@@ -1,293 +1,57 @@
-import sys
 from copy import deepcopy
+import sys
+
 read = sys.stdin.readline
-
-
-cctv = (1, 2, 3, 4, 5)
 N, M = map(int, read().split())
-area = []
-target = []
-cctv_cnt = 0
+area_main = []
+cctv_loc = []
 for i in range(N):
-    tmp = list(map(int, read().split()))
-    area.append(tmp)
-    for j in range(len(tmp)):
-        if tmp[j] in cctv:
-            target.append((i, j))
-
-def check(area, dir, y, x):
-    print("fini", area)
-    print("aaa",area[y])
-    print("bbb",area[y][x])
-    num = area[y][x]
-    if num == 1:
-        if dir == 1:
-            for i in range(y-1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-        elif dir == 2:
-            for i in range(x+1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-
-                        area[y][i] = -1
-                else:
-                    break
-        elif dir == 3:
-            for i in range(y+1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-
-                else:
-                    break
-        elif dir == 4:
-            for i in range(x-1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-
-                else:
-                    break
-    if num == 2:
-        if dir == 1 or dir ==3:
-            for i in range(y-1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(y+1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-        elif dir == 2 or dir ==4:
-            for i in range(x+1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(x-1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-
-    if num == 3:
-        if dir == 1:
-            for i in range(y-1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(x+1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-        elif dir == 2:
-            for i in range(x+1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y+1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-
-
-        elif dir == 3:
-            for i in range(y+1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(x-1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-        elif dir == 4:
-            for i in range(x-1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y-1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-    if num == 4:
-        if dir == 1:
-            for i in range(y - 1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(x + 1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y + 1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-
-        elif dir == 2:
-            for i in range(x + 1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y + 1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(x - 1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-
-        elif dir == 3:
-            for i in range(y + 1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(x - 1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y - 1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-
-        elif dir == 4:
-            for i in range(x - 1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-            for i in range(y - 1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-            for i in range(y + 1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-    if num == 5:
-        if dir == 1 or dir == 2 or dir==3 or dir ==4:
-            for i in range(y-1, -1, -1):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-
-            for i in range(x+1, M):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-            for i in range(y+1, N):
-                if area[i][x] != 6:
-                    if not area[i][x] in cctv:
-                        area[i][x] = -1
-                else:
-                    break
-
-            for i in range(x-1, -1, -1):
-                if area[y][i] != 6:
-                    if not area[y][i] in cctv:
-                        area[y][i] = -1
-                else:
-                    break
-
-cp_area = deepcopy(area)
+    tmp_area = list(map(int, read().split()))
+    area_main.append(tmp_area)
+    for j in range(M):
+        if tmp_area[j] == 1 or tmp_area[j] == 2 or tmp_area[j] == 3 or tmp_area[j] == 4 or tmp_area == 5:
+            cctv_loc.append([tmp_area[j],i, j])
+look = [[], [[0], [1], [2], [3]], [[0, 2], [1, 3]], [[0,1],[1,2],[2,3],[3,0]],[[0,1,2], [1,2,3],[2,3,0], [3,0,1]], [[0, 1, 2, 3]]]
 cnt = 0
-ordered_list = [0 for _ in range(len(target))]
-def make_order(cnt):
+dir = [[-1,0], [0, 1], [1, 0], [0, -1]]
+ans = sys.maxsize
+visit = [0 for _ in range(len(cctv_loc))]
+print(len(cctv_loc))
+def sol(cnt, area):
 
-    if cnt == len(target):
+    if cnt == len(cctv_loc):
 
-        area = deepcopy(cp_area)
-        print(area)
-
-        for i in range(len(target)):
-
-            y, x = target[i]
-            # print(target[i])
-
-            check(area, ordered_list[i], y, x)
-            # for i in range(N):
-            #     print(area[i])
-
+        global ans
+        tmp = 0
+        for i in range(N):
+            for j in range(M):
+                if area[i][j] == 0:
+                    tmp += 1
+        # print(tmp)
+        ans = min(ans, tmp)
+        print(ans)
+        for i in range(N):
+            print(area[i])
+        print()
+        return
     else:
-        for i in range(1, 5):
-
-            ordered_list[cnt] = i
-            cnt += 1
-            make_order(cnt)
-            cnt -= 1
-
-make_order(cnt)
-
-check(area[1][1], 2, 1,1)
-print(area)
-
-#
-# for i in range(N):
-#     for j in range(M):
-#         check(area[i][j], 1, i, j)
-#
-# print(area)
+        for i in range(len(cctv_loc)):
+            which_cctv, y, x = cctv_loc[i]
+            tmp = deepcopy(area)
+            for j in range(cnt, len(look[which_cctv])):
 
 
+                for cd in look[tmp[y][x]][j]:
 
+                    _, y, x = cctv_loc[i]
+                    dy, dx = dir[cd]
+                    tmp_y = y + dy
+                    tmp_x = x + dx
+                    while 0 <= tmp_y < N and 0 <= tmp_x < M and tmp[tmp_y][tmp_x] != 6:
 
+                        tmp[tmp_y][tmp_x] = -1
+                        tmp_y += dy
+                        tmp_x += dx
+                    sol(cnt + 1, tmp)
+
+sol(0, area_main)
