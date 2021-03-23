084ba6e187dfeff1efd612daef8b3e1e57195839
SeungJu <sjoh0704@daum.net>
2021-03-23 11:43:00 +0900

    solved an 16236 implementation problem

--- Baekjoon/16236.py
+++ Baekjoon/16236.py
@@ -3,83 +3,54 @@ from collections import deque
 read = sys.stdin.readline
 N = int(read())
 space = []
-baby = []
-fish = list()
+shark_y, shark_x = -1, -1
+
 for i in range(N):
     tmp = list(map(int, read().split()))
     space.append(tmp)
     for j in range(N):
-        if tmp[j] == 0:
-            continue
-        elif tmp[j] == 9:
-            baby = [i, j]
-        else:
-            fish.append((tmp[j], i, j))
+        if tmp[j] == 9:
+            shark_y, shark_x = i, j
+
 
 def find_first(fish_space, y, x, size,):
     dxy = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-    queue = deque()
-    queue.append([y, x, 0])
+    queue = deque([[y, x, 0]])
     visit = [[0 for _ in range(N)]for _ in range(N)]
     can_eat = []
     while queue:
-
         cq = queue.popleft()
-        # print(cq)
         if not visit[cq[0]][cq[1]]:
             visit[cq[0]][cq[1]] = 1
             if 1 <= fish_space[cq[0]][cq[1]] < size:
                 can_eat.append(cq)
-                # print(can_eat)
-
-
             for d in dxy:
-                tmpy = cq[0] + d[0]
-                tmpx = cq[1] + d[1]
-                if 0 <= tmpx < N and 0 <= tmpy < N and fish_space[tmpy][tmpx] <= size:
-
-                    queue.append([tmpy, tmpx, cq[2] + 1])
-    can_eat.sort(key=lambda x:(x[2], x[0], x[1]))
-
+                _y = cq[0] + d[0]
+                _x = cq[1] + d[1]
+                if 0 <= _x < N and 0 <= _y < N and fish_space[_y][_x] <= size:
+                    queue.append([_y, _x, cq[2] + 1])
+    can_eat.sort(key=lambda x: (x[2], x[0], x[1]))
     return can_eat
 
 
-
-
 def sol():
-
-    size = 2
-    total_distance = 0
+    global shark_x, shark_y
+    shark_size = 2
+    total_time = 0
     eat_cnt = 0
     while True:
-
-        tmp = find_first(space, baby[0], baby[1], size)
-        if len(tmp) != 0:
-
-            y, x, dist = tmp[0]
-
-            space[baby[0]][baby[1]] = 0
-            baby[0] = y
-            baby[1] = x
-
-            total_distance += dist
-            eat_cnt += 1
-            if eat_cnt == size:
-                size += 1
-                eat_cnt = 0
-
-        else:
+        tmp = find_first(space, shark_y, shark_x, shark_size)
+        if len(tmp) == 0:
             break
-
-
-
-
-
-        print()
-        for i in range(N):
-            print(space[i])
-        print(total_distance, size)
-
-    print(total_distance)
+        _y, _x, time = tmp[0]
+        space[_y][_x] = 0
+        space[shark_y][shark_x] = 0
+        shark_y, shark_x = _y, _x
+        total_time += time
+        eat_cnt += 1
+        if eat_cnt == shark_size:
+            shark_size += 1
+            eat_cnt = 0
+    print(total_time)
 
 sol()
