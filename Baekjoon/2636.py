44b5f2874211e7730c0450fd19f19a93140c5da0
SeungJu <sjoh0704@daum.net>
2021-05-07 09:41:30 +0900

    solved a problem with bfs

--- Baekjoon/2636.py
+++ Baekjoon/2636.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 read = sys.stdin.readline
 N, M = map(int, read().split())
 area = []
@@ -6,30 +7,41 @@ for i in range(N):
     area.append(list(map(int, read().split())))
 dx = [0, -1, 0, 1, 1, 1, -1, -1]
 dy = [-1, 0, 1, 0, -1, 1, -1, 1]
-visited = [[0]*M for _ in range(N)]
-def dfs(cy, cx):
-    if visited[cy][cx] == 1:
-        return
 
-    visited[cy][cx] = 1
-    FLAG = False
-    for i in range(4):
-        nx = cx + dx[i]
-        ny = cy + dy[i]
-        if 0<= nx < M and 0<= ny < N and area[ny][nx] == 0:
-            FLAG = True
-            break
-    if not FLAG:
-        return
-    print(cy, cx)
-    for i in range(8):
-        nx = cx + dx[i]
-        ny = cy + dy[i]
-        if 1<= nx < M-1 and 1<= ny < N-1 and area[ny][nx] == 1:
-            dfs(ny, nx)
+def bfs(y = 0, x = 0):
+    cnt = 0
+    queue = deque()
+    queue.append([y, x])
+    while queue:
+        cy, cx = queue.popleft()
+        if area[cy][cx] == 1:
+            cnt += 1
+            area[cy][cx] = 0
+            continue
 
-dfs(3, 1)
+        for i in range(4):
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<= nx < M and 0 <= ny < N and visited[ny][nx] == 0:
+                visited[ny][nx] = 1
+                queue.append([ny, nx])
+
+
+    return cnt
 
 
 
-
+v = [[0] * M for _ in range(N)]
+cnt = 0
+remain = 0
+while True:
+    visited = [item[:] for item in v]
+    cnt += 1
+    check = bfs()
+    if check == 0:
+        cnt -= 1
+        break
+    else:
+        remain = check
+print(cnt)
+print(remain)
