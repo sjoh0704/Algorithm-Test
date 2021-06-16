9cbed559b4a91ea8e505b344e9b8cd534321d732
SeungJu <sjoh0704@daum.net>
2021-06-16 10:30:35 +0900

    solved

--- Baekjoon/13458.py
+++ Baekjoon/13458.py
@@ -9,10 +9,7 @@ for i in range(N):
     A[i] -= B
     if A[i] < 0:
         A[i] = 0
-
-    while A[i] > 0:
-        A[i] -= C
+    cnt += (A[i] // C)
+    if A[i] % C != 0:
         cnt += 1
-    else:
-        A[i] = 0
 print(cnt)
