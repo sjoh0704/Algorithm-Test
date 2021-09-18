c8ceb3319a46992d3672bb580ed79286656bc819
seungjuoh <sjoh0704@daum.net>
2021-09-18 11:52:18 +0900

    solved

--- Baekjoon/17837.py
+++ Baekjoon/17837.py
@@ -1,89 +1,88 @@
 import sys
+from time import sleep
 read = sys.stdin.readline
-N, K = map(int ,read().split())
-area = []
+N, K = map(int, read().split())
+color = []
 for _ in range(N):
-    area.append(list(map(int, read().split())))
-horses = []
-for _ in range(K):
-    y, x, d  = map(int ,read().split())
-    horses.append([y-1, x-1, d-1])
- 
-dy = [0, 0, -1, 1]
-dx = [1, -1, 0, 0]
+    color.append(list(map(int, read().split())))
 
-state = [[[]for _ in range(N)]for _ in range(N)]
-for i, h in enumerate(horses):
-    state[h[0]][h[1]].append([i, h[2]])
+area = [[[] for _ in range(N)] for _ in range(N)]
 
-def sol():
-    for i in range(K):
-        FLAG = True
+for i in range(1, K+1):
+    y, x, d = map(int, read().split())
+    area[y-1][x-1].append([i, d])
+
+dx = [0, 1, -1, 0, 0]
+dy = [0, 0, 0, -1, 1]
+
+def turn():
+    for i in range(1, K+1):
+
+        FLAG = False
         for j in range(N):
             for k in range(N):
-                for l, s in enumerate(state[j][k]):
-                    if s[0] == i:
-                        FLAG = False
-                        cd = s[1]        
-                        ny = j + dy[cd]
-                        nx = k + dx[cd]
-                        if not(0<=ny<N and 0<=nx<N) or area[ny][nx] == 2:
-                            nd = 1 if cd == 0 else ( \
-                            0 if cd == 1 else (\
-                            2 if cd == 3 else (\
-                            3 if cd == 2 else -1)))       
-                            ny = j + dy[nd]
-                            nx = k + dx[nd]
-                            if  not(0<=ny<N and 0<=nx<N) or area[ny][nx] == 2:
-                                state[j][k][l][1] = nd   
+                elem = area[j][k]
+                for l in range(len(elem)):
+                    if elem[l][0] == i:
+                        cdir = elem[l][1]
+                        FLAG = True
+                        ny = dy[cdir] + j
+                        nx = dx[cdir] + k
+                        if not(0<=ny<N and 0<=nx<N) or color[ny][nx] == 2:
+                      
+                            if cdir == 1:
+                                cdir = 2
+                            elif cdir == 2:
+                                cdir = 1
+                            elif cdir == 3:
+                                cdir = 4
+                            elif cdir == 4:
+                                cdir = 3
+                            ny = dy[cdir] + j
+                            nx = dx[cdir] + k
+                            area[j][k][l][1] = cdir
+
+                            if not(0<=ny<N and 0<=nx<N) or color[ny][nx] == 2:
+                                area[j][k][l][1] = cdir
                             else:
-
-                                if area[ny][nx] == 0:
-                                    state[ny][nx].extend(state[j][k][l:])
-                                    state[j][k] = state[j][k][:l]
-                                elif area[ny][nx] == 1:
-                                    state[ny][nx].extend(state[j][k][l:][::-1])
-                                    state[j][k] = state[j][k][:l]
-                                if len(state[ny][nx]) == K:
-                 
+                                if color[ny][nx] == 1:
+                                    area[ny][nx].extend(elem[l:][::-1])
+                                    area[j][k] = elem[:l]
+                                elif color[ny][nx] == 0:
+                                    area[ny][nx].extend(elem[l:])
+                                    area[j][k] = elem[:l]
+                                if len(area[ny][nx]) >= 4:
                                     return 1
-
                         else:
-                            if area[ny][nx] == 0:
-                                state[ny][nx].extend(state[j][k][l:])
-                                state[j][k] = state[j][k][:l]
-                            elif area[ny][nx] == 1:
-                                state[ny][nx].extend(state[j][k][l:][::-1])
-                                state[j][k] = state[j][k][:l]
-                            print(state[ny][nx])
-                            if len(state[ny][nx]) == K:
+                            if color[ny][nx] == 1:
+                                area[ny][nx].extend(elem[l:][::-1])
+                                area[j][k] = elem[:l]
+                            elif color[ny][nx] == 0:
+                                area[ny][nx].extend(elem[l:])
+                                area[j][k] = elem[:l]
+                            if len(area[ny][nx]) >= 4:
                                 return 1
-                        break
-                    
-                if not FLAG:
-                    break
 
-            if not FLAG:
+                    if FLAG:    
+                        break
+                if FLAG:
+                    break
+            if FLAG:
                 break
     return -1
+
 cnt = 0
-while True:
-    cnt += 1
-    check = sol()
+while 1:
+    cnt += 1  
+    check = turn()
+
     if check == 1:
         print(cnt)
-        break 
-    if cnt > 1000:
+        break
+    elif cnt > 1000:
         print(-1)
         break
-
-    
-
-# for s in state:
-#     print(s)
-
-
-
-
-
-
+  
+# for a in area:
+#     print(a)                        
+# print()
