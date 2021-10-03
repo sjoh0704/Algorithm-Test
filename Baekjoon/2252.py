3525a50b9fa1f1f7a3f034ac35ca7673a75afbd4
seungjuoh <sjoh0704@daum.net>
2021-10-04 00:16:53 +0900

    fail

--- Baekjoon/2252.py
+++ Baekjoon/2252.py
@@ -4,4 +4,28 @@ N, M = map(int, read().split())
 case = []
 for _ in range(M):
     case.append(list(map(int ,read().split())))
-print(case)
+cnt = [0] * (N+1)
+stack = []
+for c1, c2 in case:
+    cnt[c2] += 1
+for i in range(1, N+1):
+    if cnt[i] == 0:
+        stack.append(i)
+        cnt[i] = 'x'
+ans = []
+while stack:
+    cp = stack.pop()
+    ans.append(cp)
+    for c1, c2 in case:
+        if c1 == cp:
+            cnt[c2] -= 1
+    for i in range(1, N+1):
+        if cnt[i] == 0:
+            stack.append(i)
+            cnt[i]='x'
+    
+for a in ans:
+    print(a, end=' ')
+print()
+
+
