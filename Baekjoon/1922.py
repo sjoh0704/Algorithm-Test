746ca4e55add4defbcaff6bf6fe38661f4f43d3e
seungjuoh <sjoh0704@daum.net>
2021-10-06 11:43:27 +0900

    solved

---
+++ Baekjoon/1922.py
@@ -0,0 +1,41 @@
+import sys
+read = sys.stdin.readline
+N = int(read())
+M = int(read())
+case = []
+for _ in range(M):
+    case.append(list(map(int ,read().split())))
+
+sorted_case = sorted(case, key=lambda x:x[2]) 
+# print(sorted_case)
+parents = [i for i in range(N+1)]
+def find(x):
+    if parents[x] == x:
+        return x    
+        
+    parents[x] = find(parents[x])
+
+    return parents[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x > y:
+        parents[x] = y
+    elif x < y:
+        parents[y] = x
+
+def same_parents(x, y):
+    x = find(x)
+    y = find(y)
+    if x == y:
+        return True
+    return False
+
+ans = 0
+for a, b, v in sorted_case:
+    if not same_parents(a, b):
+        union(a, b)
+        ans += v
+print(ans)
+
