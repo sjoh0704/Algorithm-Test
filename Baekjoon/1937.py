04196bb44f0b754ae2ed8746108e23b20d1187a7
seungjuoh <sjoh0704@daum.net>
2021-10-09 12:20:48 +0900

    solved

--- Baekjoon/1937.py
+++ Baekjoon/1937.py
@@ -1,35 +1,34 @@
 import sys
+# sys.setrecursionlimit(10**6)
 read = sys.stdin.readline
 n = int(read())
-area = []
+graph = []
 for _ in range(n):
-    area.append(list(map(int, read().split())))
-
+    graph.append(list(map(int ,read().split())))
 dp = [[0 for _ in range(n)] for _ in range(n)]
-dy =[0, 0, 1, -1]
-dx =[1, -1 ,0, 0]
-
-def dfs(cy, cx, area, visited=[]):
-    if dp[cy][cx] == 0:
-        dp[cy][cx] = 1
-    
-    visited.append([cy, cx])
-    print(cy, cx)
-    
+dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
+def dfs(y, x):
+    if dp[y][x]:
+        return dp[y][x]
+    dp[y][x] = 1
+    # visited.append([y,x])
+    # print(y, x)
     for i in range(4):
-        ny = cy + dy[i]
-        nx = cx + dx[i]
-        if 0 <= ny < n and 0 <= nx < n and [ny, nx] not in visited:
-            if area[ny][nx] > area[cy][cx]:
-                dfs(ny, nx, area, visited)
-                dp[ny][nx] = max(dp[cy][cx] + 1, dp[ny][nx]) 
-
-# dfs(1, 0, area)
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0<= nx < n and 0 <= ny < n :
+            if graph[ny][nx] > graph[y][x]:
+                # pdb.set_trace()
+                dp[y][x] = max(dp[y][x], dfs(ny, nx) + 1)
+    return dp[y][x]
+    
+result = 0
 for i in range(n):
     for j in range(n):
-        _area = area.copy()
-        print("--------------------------------")
-        dfs(i, j, _area)
-        for d in dp:
-            print(d)
-        print()
+        # print(i, j, "------------------")
+        result = max(result, dfs(i, j))
+        # for d in dp:
+        #     print(d)
+        # print()
+print(result)
