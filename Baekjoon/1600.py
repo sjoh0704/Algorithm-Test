0165f1dae8667a901bdb30cbffa5b8c9c7d6c61d
seungjuoh <sjoh0704@daum.net>
2021-10-10 14:30:10 +0900

    add vistied

--- Baekjoon/1600.py
+++ Baekjoon/1600.py
@@ -2,6 +2,7 @@ import sys
 from collections import deque
 read = sys.stdin.readline
 # 말처럼 이동
+MAX = 10000000
 h_dx = [-1, -2, -2, -1, 1, 2, 2, 1]
 h_dy = [-2, -1, 1, 2, 2, 1, -1, -2]
 
@@ -16,31 +17,39 @@ area = []
 for _ in range(h):
     area.append(list(map(int ,read().split())))
 
-print(area)
 
 def bfs():
-    ans = 100000000
+    ans = MAX
     queue = deque()
     queue.append([0, 0, 0, k])
-    visited = [[0 for _ in range(w)] for _ in range(h)]
-    
-    
+    visited = [[[0 ,0] for _ in range(w)] for _ in range(h)]
+    visited[0][0] = [1, 1]
     while queue:
         cy, cx, cc, ck = queue.popleft()
-        if visited[cy][cx]:
-            continue
-        visited[cy][cx] = 1
-        
+        # print(cy, cx, cc, ck)
         if cy == h-1 and cx == w-1:
             ans = min(ans, cc)
             continue
-        print(cy ,cx, cc, ck)
+        # print(cy ,cx, cc, ck)
+
+        if ck > 0:
+            _ck = ck - 1
+            for i in range(8):
+                ny = cy + h_dy[i]
+                nx = cx + h_dx[i]
+                if 0 <= ny < h and 0<= nx < w and not area[ny][nx] and not visited[ny][nx][0]:
+                    queue.append([ny ,nx, cc+1, _ck])
+                    visited[ny][nx][0] = 1
         for i in range(4):
             ny = cy + dy[i]
             nx = cx + dx[i]
-            if 0 <= ny < h and 0<= nx < w:
-                queue.append([ny ,nx, cc+1, ck-1])
+            if 0 <= ny < h and 0<= nx < w and not area[ny][nx] and not visited[ny][nx][1]:
+                queue.append([ny ,nx, cc+1, ck])
+                visited[ny][nx][1] = 1
 
     return ans
 ans = bfs()
-print(ans)
+if ans == MAX:
+    print(-1)
+else:
+    print(ans)
