dc7d4d9a52abf2aeb0d3bebeba88230ac934e2f3
seungjuoh <sjoh0704@daum.net>
2021-10-13 11:31:40 +0900

    solved

--- Baekjoon/1916.py
+++ Baekjoon/1916.py
@@ -2,38 +2,29 @@ import sys
 read = sys.stdin.readline
 N = int(read())
 M = int(read())
-MAX = 100000000
+MAX = int(1e9)
 cost = [[0 for _ in range(N+1)] for _ in range(N+1)]
 for _ in range(M): 
     s, d, c = map(int, read().split())
     cost[s][d] = c
 src, dest = map(int, read().split())
-
 graph = [0 for _ in range(N+1)]
-
-# 초기값 설정
 for i in range(N+1):
     if cost[src][i]:
-        graph[i] = cost[1][i]
+        graph[i] = cost[src][i]
     else:
         graph[i] = MAX
-# print(graph)
-
 exclude = [src]
-min_idx = 0
-
-
 while len(exclude) < N:
-    # print(graph)
+    min_idx = 0
     for i in range(1, N+1):
         if graph[min_idx] > graph[i] and i not in exclude:
             min_idx = i
-    
-    for i in range(1, N+1):
-        if i not in exclude:
+    for i, c in enumerate(cost[min_idx]):
+        if c:
             graph[i] = min(graph[i], graph[min_idx] + cost[min_idx][i])
-
     exclude.append(min_idx)
-
-
-print(graph[dest])
+if graph[dest] == MAX:
+    print(0)
+else:
+    print(graph[dest])
