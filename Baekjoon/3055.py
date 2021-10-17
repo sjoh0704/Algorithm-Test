205fc6dae7a627cb07a3c3f781fe7dab712b243a
seungjuoh <sjoh0704@daum.net>
2021-10-17 12:15:26 +0900

    out of memory

--- Baekjoon/3055.py
+++ Baekjoon/3055.py
@@ -8,49 +8,68 @@ water = []
 start = None
 for i in range(R):
     tmp = list(read())[:-1]
-    print(tmp)
+  
     area.append(tmp)
     for j in range(C):
         if tmp[j] == 'S':
-            start = [i, j, 0]
+            start = [i, j, 'S']
         elif tmp[j] == '*':
-            water.append([i, j, 0])
+            water.append([i, j, '*'])
     
-print(area)
-print(water)
-print(start)
 queue = deque()
-visited = [[[0, 0]for _ in range(C)]for _ in range(R)]
+cnt = deque()
+# visited = [[ 0 for _ in range(C)]for _ in range(R)]
+visited = []
 for w in water:
     queue.append(w)
 queue.append(start)
-
+cnt.append(0)
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
 ans = inf
 while queue:
-    cy, cx, cc = queue.popleft()
-    if area[cy][cx] == '*' and not visited[cy][cx][0]:
-        visited[cy][cx][0] = 1
+    cy, cx, ch= queue.popleft()
+    # print(cy, cx, cc)
+    
+    if ch == '*':
+        # print('*',cy, cx, cc)
+        # print(queue)
+        area[cy][cx]= '*'
         for i in range(4):
             ny = cy + dy[i]
             nx = cx + dx[i]
             if 0<= ny < R and 0<= nx < C:
-                if area[ny][nx] != 'D' and area[ny][nx] != 'X':
-                    area[ny][nx]= '*'
-                    queue.append([ny, nx, 0])
-    elif area[cy][cx] != 'X' and not visited[cy][cx][1]:
-        if area[cy][cx] == 'D':
-            ans = min(anx, cc)
-            print(ans)
+                if area[ny][nx] != 'D' and area[ny][nx] != 'X' and area[ny][nx] != '*':
+                    queue.append([ny, nx, '*'])
+                
 
-        visited[cy][cx][1] = 1
+
+    elif ch =='S' and [cy,cx] not in visited:
+        cc = cnt.popleft()
+        # print(cnt)
+        # print('s',cy, cx, cc)
+        # print(queue)
+        if area[cy][cx] == '*':
+            continue
+
+
+        if area[cy][cx] == 'D':
+            ans = min(ans, cc)
+            # print(ans)
+
+        visited.append([cy,cx])
         for i in range(4):
             ny = cy + dy[i]
             nx = cx + dx[i]
             if 0<= ny < R and 0<= nx < C:
                 if area[ny][nx] != '*' and area[ny][nx] != 'X':
-                    area[ny][nx]= '*'
-                    queue.append([ny, nx, cc+1])
+                    
+                    queue.append([ny, nx, 'S'])
+                    cnt.append(cc+1)
+    else:
+        cc = cnt.popleft()
 
-print(ans)
+if ans == inf:
+    print('KAKTUS')
+else:
+    print(ans)
