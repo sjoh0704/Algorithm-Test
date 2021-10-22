a3c9d9db8f75fa0bb58790be3959a24c27c10f20
seungjuoh <sjoh0704@daum.net>
2021-10-22 23:27:18 +0900

    solved

--- Baekjoon/2859.py
+++ Baekjoon/2859.py
@@ -5,17 +5,12 @@ r, c = map(int ,read().split())
 area = []
 for _ in range(r):
     area.append(list(read())[:-1])
-# print(area)
-
-visited = [[ 0 for _ in range(c)]for _ in range(r)]
-
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0 ,0]
-
-
 def bfs(sy, sx):
+    visited = [[ 0 for _ in range(c)]for _ in range(r)]
     global target 
-    queue = deque([[sy, sx, 1]])
+    queue = deque([[sy, sx, 0]])
     while queue:
         cy, cx, cc = queue.popleft()    
         if visited[cy][cx]:
@@ -27,15 +22,10 @@ def bfs(sy, sx):
             nx = cx + dx[i]
             if 0<=ny <r and 0<=nx < c:
                 if area[ny][nx] == 'L':
-                    queue.append([ny, nx, cc+1])
-
-                
-
+                    queue.append([ny, nx, cc+1])            
 target = 0
 for i in range(r):
     for j in range(c):
-        if area[i][j] == 'L' and not visited[i][j]:
-            # print(i, j)
+        if area[i][j] == 'L':
             bfs(i, j)
-            # print(target)
 print(target)
