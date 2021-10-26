19d12f384317660db1d91ce5d4bc586f93f66829
seungjuoh <sjoh0704@daum.net>
2021-10-26 23:45:51 +0900

    runtime

--- Baekjoon/1976.py
+++ Baekjoon/1976.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 read = sys.stdin.readline
 N = int(read())
@@ -12,20 +13,28 @@ for i in range(1, M+1):
             else:
                 dic[i].append(j)
 
-print(dic)
-move = list(map(int, read().split()))
-print(move)
+# print(dic)
+move = deque(list(map(int, read().split())))
+# print(move)
 
 
 
 def dfs(start):
     stack = [start]
 
-    while stack:
+    while move and stack:
         cs = stack.pop()
-        print(cs)
+        # print(cs)
+        if move[0] == cs:
+            x = move.popleft()
+            # print(x)
         for ns in dic[cs]:
             stack.append(ns)
-        
+    
+    if move:
+        print("FALSE")
+    else:
+        print("YES")
+
 dfs(move[0])
 
