c0f36e5f600ba72126956a775f32c869f93207c8
seungjuoh <sjoh0704@daum.net>
2021-10-27 01:09:22 +0900

    not solved

--- Baekjoon/1976.py
+++ Baekjoon/1976.py
@@ -1,40 +1,61 @@
-from collections import deque
 import sys
 read = sys.stdin.readline
 N = int(read())
 M = int(read())
-dic = {}
+
+parents = [i for i in range(N+1)]
+
+area = []
 for i in range(1, M+1):
     tmp = list(map(int ,read().split()))
-    for j in range(1, N+1):
-        if tmp[j-1]: 
-            if i not in dic:
-                dic[i] = [j]
-            else:
-                dic[i].append(j)
+    area.append(tmp)
 
-# print(dic)
-move = deque(list(map(int, read().split())))
-# print(move)
+move = list(map(int, read().split()))
 
+def find(x):
+    if x == parents[x]:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
 
-
-def dfs(start):
-    stack = [start]
-
-    while move and stack:
-        cs = stack.pop()
-        # print(cs)
-        if move[0] == cs:
-            x = move.popleft()
-            # print(x)
-        for ns in dic[cs]:
-            stack.append(ns)
-    
-    if move:
-        print("FALSE")
+def union(x, y):
+    a = parents[x]
+    b = parents[y]
+    if a < b:
+        parents[y] = a
     else:
-        print("YES")
+        parents[x] = b
 
-dfs(move[0])
+def same_parents(x, y):
+    x = parents[x]
+    y = parents[y]
+    if x==y:
+        return True
+    else:
+        return False
+# print()
+for i in range(N):
+    for j in range(N):
+        if area[i][j]:
+            if not same_parents(i+1, j+1):
+                union(i+1, j+1)
+for i in range(N):
+    for j in range(N):
+        if area[i][j]:
+            if not same_parents(i+1, j+1):
+                union(i+1, j+1)
+
+
+
+FLAG = True
+for i in range(len(move)-1):
+    if not same_parents(move[i], move[i+1]):
+        FLAG = False
+        break
+if FLAG:
+    print("YES")
+else:
+    print("FALSE")
+
+
 
