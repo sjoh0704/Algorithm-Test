7f5e2516e1c034bd2f09a32b20197ec08b2b37de
seung <sjoh0704@daum.net>
2021-12-18 09:13:33 +0900

    fix

--- Baekjoon/2748.py
+++ Baekjoon/2748.py
@@ -1,7 +1,10 @@
 import sys
 n = int(sys.stdin.readline())
-fivo = [0 for _ in range(n+1)]
-fivo[1], fivo[2] = 1, 1
-for i in range(3, n+1):
-    fivo[i] = fivo[i-1] + fivo[i-2]
-print(fivo[n])
+if n < 2:
+    print(n)
+else:
+    fivo = [0 for _ in range(n+1)]
+    fivo[1], fivo[2] = 1, 1
+    for i in range(3, n+1):
+        fivo[i] = fivo[i-1] + fivo[i-2]
+    print(fivo[n])
