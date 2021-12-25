cfc88a0945d118feb78180987a5d0f1198d6b534
seung <sjoh0704@daum.net>
2021-12-25 11:03:02 +0900

    checked

--- Go/2167.go
+++ Go/2167.go
@@ -3,43 +3,43 @@ package main
 import (
 	"fmt"
 )
+
+var area [301][301] int
 var dp [301][301] int 
 
 func main(){
 	var a, b, x, y int
 	var k int
-	setNums()
-	fmt.Scan(&k)
-	result := make([]int, k)
 	
+	setNums()
+	
+	fmt.Scan(&k)
+
 	for i:=0; i < k; i++{
 		fmt.Scan(&a, &b, &x, &y)
-		result[i] = sol(a, b, x, y)
+		fmt.Println(sol(a, b, x, y))
 	}
-	
-	for i:=0; i<k; i++{
-		fmt.Println(result[i])
-	}
-
 }
 
+// 차원 배열 초기값, dp 세팅
 func setNums(){
 	var n, m int
 	fmt.Scan(&n, &m)
 	for i:=1; i<n+1; i++{
 		for j:=1; j <m+1; j++{
-			var tmp int
-			fmt.Scan(&tmp)
-			for k:= i; k < n+1; k++{
-				for l:=j; l < m+1; l++{
-					dp[k][l] += tmp
-				}
-			}
+			fmt.Scan(&area[i][j])
+		}
+	}
+
+	// dp 값 세팅 
+	for i:=1; i<n+1; i++{
+		for j:=1; j<m+1; j++{
+			dp[i][j] = area[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 		}
 	}
 }
 
-func sol(a int, b int, x int, y int) int {
-	sum := dp[x][y] - dp[x][b-1] - dp[a-1][y] + dp[a-1][b-1]
-	return sum
+// dp 값 이용해서 결과값 구하기 
+func sol(i int, j int, x int, y int) int {
+	return dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1]
 }
